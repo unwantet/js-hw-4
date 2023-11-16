d71a0d28716ac2b4e6dce8e2beba20f808cf1bff
unwantet <darkblezer59@gmail.com>
2023-11-16 19:44:36 +0500

    added 4 -hw

---
+++ main.js
@@ -0,0 +1,118 @@
+
+//Karra jadvali :
+
+
+// let result = '   ';
+// for (let i = 0; i < 11; i++) {
+
+//     for (let j = 0; j < 11; j++) {
+
+//         if(i == 0 && j > 0){
+//           result += '[' + j + ']';
+//         } 
+//         else if(j == 0 && i>0){
+//           result += '[' + i + '] ';
+//         } 
+//         else if(i>0 && j>0){
+//         result += (i*j) + ' ';
+//         }
+//     }
+//     result += '\n'
+// }
+// console.log(result);
+
+//////////////////////////////////////
+
+
+// 1 
+
+// for (let a = 1; a<=10; a++ ){
+//   console.log(a)
+// }
+
+/////////////////////////
+
+//2
+
+// for ( let a = 1; a<=100 ; a++ ){
+//     if (a%2==1){
+//         console.log(a)
+//     }
+// }
+
+
+///////////////////////////
+
+// for ( let a = 10; a<=30 ; a++ ){
+//     if (a%2==0){
+//         console.log(a)
+//     }
+// }
+
+
+//////////////////////////////////////
+
+// let a = +prompt("Selsiyda son kirt");
+// let b = a * 5/9 + 32;
+// let c = " gradus bolad Farengeytta"
+// alert(b +c)
+
+
+//////////////////////////////
+
+// let a = +prompt("farengeyta son kirt");
+// let b = (a -32)* (5/9);
+// let c = " gradus bolad selsiyda"
+// alert(b +c)
+
+///////////////////////////////////
+
+// let r = +prompt("1 - sonni kirt");
+// let n = +prompt("2 - sonni kirt");
+// for (let a = 1; a<=n; a++ ){
+//     console.log(r);
+// }
+
+
+///////////////////////////////////////////
+
+// let r = +prompt("1 - sonni kirt");
+// let n = +prompt("2 - sonni kirt");
+// for (let a = r; a<=n; a++ ){
+//     console.log(a);
+// }
+
+/////////////////////////////////////////
+
+
+// let r = +prompt("1 - sonni kirt");
+// let n = +prompt("2 - sonni kirt");
+// for (let a = n; a > r; a--){
+//     console.log(a);
+// }
+
+
+//////////////////////////////////////////////
+
+
+// let b = +prompt("kanfet narxini kiriting!");
+// for (let a = 1 ; a<10; a++){
+//   console.log(a*b + " ming som");
+// }
+
+////////////////////////////////////////////////////
+
+// let b = +prompt("kanfet narxini kiriting!");
+// for (let a = 0 ; a<=1; a= a + 0.1){
+//   console.log(a*b + " ming som");
+// }
+
+
+////////////////////////////////////////////
+
+// let b = +prompt("kanfet narxini kiriting!");
+// for (let a = 1 ; a<=2; a= a + 0.2){
+//   console.log(a*b + " ming som");
+// }
+
+////////////////////////////////////////////////////
